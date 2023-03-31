fd430a27d683998cdc3b26147033876219f267f4
Andrew Bentzen <bentzen.an@gmail.com>
2023-03-31 10:54:01 -0400

    destroys bullet when it hits something or after a time limit.

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -9,13 +9,26 @@ public class BulletController : MonoBehaviour {
     [SerializeField]
     private Rigidbody rb;
 
+    [SerializeField]
+    private float lifetime = 5.0f;
+
     // Start is called before the first frame update
     void Start() {
         
     }
 
+
     // Update is called once per frame
     void Update() {
-        rb.velocity = transform.forward*moveSpeed;
+	rb.velocity = transform.forward * moveSpeed;
+
+	// destroy bullets after x seconds
+	lifetime -= Time.deltaTime;
+	if (lifetime <= 0.0f) Destroy(gameObject);
+    }
+
+    
+    private void OnTriggerEnter(Collider other) {
+	Destroy(gameObject);
     }
 }
